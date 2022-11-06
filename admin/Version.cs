8f58ca3b94867360677d84d6135e1a2a0eaf9f5e
gulshan <gulshanjitm@gmail.com>
2022-11-06 19:56:34 +0530

    #1 Initial Creation of project structure

---
+++ admin/Version.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("BBSC")]
+[assembly: AssemblyProduct("BBSC Accounts Wonders")]
+[assembly: AssemblyCopyright("Copyright to BBSC © 2020")]
+[assembly: AssemblyVersion("1.0.0.0")]
